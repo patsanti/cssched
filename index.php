8fe5880b97ab76c719cca25c56cb333a521f48d5
AustinZuniga <earlaustinavilazuniga@gmail.com>
2018-07-11 15:45:11 +0800

    restructure project codes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/all.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 <link href="css/main.css" rel="stylesheet" type="text/css"/>
 <link rel="icon" href="bucs-logo.png">
 <title>CSIT Schedule</title>
--- php/connect.php
+++ php/connect.php
@@ -2,13 +2,13 @@
 
 define("DB_SERVER", "localhost"); //host you want to connect
 define("DB_USER", "root"); //database user
-define("DB_PASS", "password"); //db password
+define("DB_PASS", ""); //db password
 define("DB_NAME", "cs_classsched"); //database name
 
 $connect = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME)
 			or die('Error: '.mysqli_connect_error());
 
-$conn = new mysqli('localhost','root','password','cs_classsched');
+$conn = new mysqli('localhost','root','','cs_classsched');
 if($conn->connect_errno){
     echo $conn->connect_error;
     die('404 Database not found.');
