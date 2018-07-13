e9e416e889b9657234c533dfd176d304f1d21404
AustinZuniga <earlaustinavilazuniga@gmail.com>
2018-07-13 16:16:32 +0800

    added: add schedule function and CSP for scheduling

--- js/script.js
+++ js/script.js
@@ -1,66 +1,65 @@
 $(document).ready(function() {
-               $.ajax({
-                type: "POST",
-                url: "php/functions.php",
-                data: {
-                  get_course: "1"
-                 
-                },
-                success: function(result) {
-                    document.getElementById("select-course").innerHTML = result;       
-                },
-                error: function(result) {
+// get list of courses
+     $.ajax({
+      type: "POST",
+      url: "php/functions.php",
+      data: {
+        get_course: "1"
+       
+      },
+      success: function(result) {
+          document.getElementById("select-course").innerHTML = result;       
+      },
+      error: function(result) {
 
-                   }
-              });
+         }
+    });
+// get list of professors
+    $.ajax({
+      type: "POST",
+      url: "php/functions.php",
+      data: {
+        get_prof: "1"
+       
+      },
+      success: function(result) {
+          document.getElementById("select-prof").innerHTML = result;       
+      },
+      error: function(result) {
 
-              $.ajax({
-                type: "POST",
-                url: "php/functions.php",
-                data: {
-                  get_prof: "1"
-                 
-                },
-                success: function(result) {
-                    document.getElementById("select-prof").innerHTML = result;       
-                },
-                error: function(result) {
+         }
+    });
 
-                   }
-              });
+// get list of class
+    $.ajax({
+      type: "POST",
+      url: "php/functions.php",
+      data: {
+        get_class: "1"
+       
+      },
+      success: function(result) {
+          document.getElementById("select-class").innerHTML = result;       
+      },
+      error: function(result) {
 
+         }
+    });
+// get list of room
+     $.ajax({
+      type: "POST",
+      url: "php/functions.php",
+      data: {
+        get_room: "1"
+       
+      },
+      success: function(result) {
+          document.getElementById("select-room").innerHTML = result;       
+      },
+      error: function(result) {
 
-              $.ajax({
-                type: "POST",
-                url: "php/functions.php",
-                data: {
-                  get_class: "1"
-                 
-                },
-                success: function(result) {
-                    document.getElementById("select-class").innerHTML = result;       
-                },
-                error: function(result) {
-
-                   }
-              });
-
-               $.ajax({
-                type: "POST",
-                url: "php/functions.php",
-                data: {
-                  get_room: "1"
-                 
-                },
-                success: function(result) {
-                    document.getElementById("select-room").innerHTML = result;       
-                },
-                error: function(result) {
-
-                   }
-              });
-
-
-
-
+         }
+    });
+     
+    
 });
