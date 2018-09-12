c76ce3e14124ebb963b33fc4fc1697b62c53a1d6
AustinZuniga <earlaustinavilazuniga@gmail.com>
2018-09-12 19:34:02 +0800

    redesign pages

--- department/request/js/script.js
+++ department/request/js/script.js
@@ -1,131 +1,129 @@
  var dp = new DayPilot.Calendar("dp");
-    dp.startDate = "2013-03-04";  // DO NOT CHANGE!!
-    dp.viewType = "Week";
+dp.startDate = "2013-03-04";  // DO NOT CHANGE!!
+dp.viewType = "Week";
     
-    // event creating schedule
-    dp.onTimeRangeSelected = function (args) {
-        var subject = document.getElementById('select-course').value;
-        var professor = document.getElementById('select-prof').value;
-        var class_data = document.getElementById('select-class').value;
-        var room = document.getElementById('select-room').value;
-        $.ajax({
-            type: "POST",
-            url: "php/functions.php",
-            data: {
-                get_schedule_data: 1,
-                get_subject_1: subject,
-                get_professor_1: professor,
-                get_class_1: class_data,
-                get_room_1: room
-            },
-            success: function(result) {
 
-                var name = result;
-                if (!name) return;
-                var e = new DayPilot.Event({
-                    start: args.start,
-                    end: args.end,
-                    id: DayPilot.guid(),
-                    text: name
-                });
-                var start_time = args.start.toString();
-                var end_time = args.end.toString();
-                start_time = start_time.split("T");
-                end_time = end_time.split("T");
-                var day = start_time[0].split("-");
-                $.ajax({
-                    type: "POST",
-                    url: "php/functions.php",
-                    data: {
-                        add_schedule: 1,
-                        start_time: start_time[1],
-                        end_time: end_time[1],
-                        subject: subject,
-                        professor: professor,
-                        class_1: class_data,
-                        room: room,
-                        day: day[2]
+dp.headerDateFormat = "dddd";
+dp.init();
 
-                    },
-                    success: function(result) {
-                        if(result == 1){
-                            dp.events.add(e);
-                            dp.clearSelection();
-                            $("#error_msg").css({ color: 'green' });
-                            document.getElementById('error_msg').innerHTML = " Schedule Added";
-                            window.setTimeout(function(){ window.location = "index.html"; },1000);
-                        }
-                        else{
-                            $("#error_msg").css({ color: 'red' });
-                            document.getElementById('error_msg').innerHTML = "Schedule/Subject Already Taken!";
-                        }
-                        
+// for getting all schedule data and displays
+$.ajax({
+    type: "POST",
+    url: "php/functions.php",
+    data: {
+        get_schedule_data_all: 1
+    },
+    success: function(result) {
+        var schedule = eval(result);
+        var Length = schedule.length / 4;
+        var n = 0;
+
+        for (var i = 0; i < Length; i++){
+            var e = new DayPilot.Event({
+                start: new DayPilot.Date(schedule[n+2]),
+                end: new DayPilot.Date(schedule[n+3]),
+                id: schedule[n],
+                text: schedule[n+1],
+            });
+            dp.events.add(e);
+            n = n + 4;
+        }
+        
+    }
+});
+
+
+// event creating schedule
+dp.onTimeRangeSelected = function (args) {
+    var subject = document.getElementById('select-course').value;
+    var professor = document.getElementById('select-prof').value;
+    var class_data = document.getElementById('select-class').value;
+    var room = document.getElementById('select-room').value;
+    $.ajax({
+        type: "POST",
+        url: "php/functions.php",
+        data: {
+            get_schedule_data: 1,
+            get_subject_1: subject,
+            get_professor_1: professor,
+            get_class_1: class_data,
+            get_room_1: room
+        },
+        success: function(result) {
+
+            var name = result;
+            if (!name) return;
+            var e = new DayPilot.Event({
+                start: args.start,
+                end: args.end,
+                id: DayPilot.guid(),
+                text: name
+            });
+            var start_time = args.start.toString();
+            var end_time = args.end.toString();
+            start_time = start_time.split("T");
+            end_time = end_time.split("T");
+            var day = start_time[0].split("-");
+            $.ajax({
+                type: "POST",
+                url: "php/functions.php",
+                data: {
+                    add_schedule: 1,
+                    start_time: start_time[1],
+                    end_time: end_time[1],
+                    subject: subject,
+                    professor: professor,
+                    class_1: class_data,
+                    room: room,
+                    day: day[2]
+
+                },
+                success: function(result) {
+                    if(result == 1){
+                        dp.events.add(e);
+                        dp.clearSelection();
+                        $("#error_msg").css({ color: 'green' });
+                        document.getElementById('error_msg').innerHTML = " Schedule Added";
+                        window.setTimeout(function(){ window.location = "index.html"; },1000);
                     }
-                });
-                
-            }
-        });
-
-
-    };
-    // function when user clicked on the schedule
-    dp.onEventClick = function(args) {
-
-        //alert(args.e.start());
-        var start = args.e.id();
-        $.ajax({
-            type: "POST",
-            url: "php/functions.php",
-            data: {
-                show_schedule: start
-            },
-            success: function(result) {
-                var schedule = eval(result);
-                document.getElementById('course_code').innerHTML = "<b>Course Code:</b> " + schedule[0];
-                document.getElementById('course_description').innerHTML = "<b>Course Description:</b> " + schedule[1];
-                document.getElementById('professor').innerHTML = "<b>Faculty:</b> " + schedule[2];
-                document.getElementById('class').innerHTML = "<b>Class:</b> " + schedule[3];
-                document.getElementById('room').innerHTML = "<b>Room:</b> " + schedule[4];
-                document.getElementById('schedule').innerHTML = "<b>Schedule:</b> " + schedule[5];      
-                $("#myModal").modal();
-
-                
-            }
-        });
-    };
-
-    dp.headerDateFormat = "dddd";
-    dp.init();
-
-    // for getting all schedule data and displays
-        $.ajax({
-            type: "POST",
-            url: "php/functions.php",
-            data: {
-                get_schedule_data_all: 1
-            },
-            success: function(result) {
-                var schedule = eval(result);
-                var Length = schedule.length / 4;
-                var n = 0;
-
-                for (var i = 0; i < Length; i++){
-                    var e = new DayPilot.Event({
-                        start: new DayPilot.Date(schedule[n+2]),
-                        end: new DayPilot.Date(schedule[n+3]),
-                        id: schedule[n],
-                        text: schedule[n+1],
-                    });
-                    dp.events.add(e);
-                    n = n + 4;
+                    else{
+                        $("#error_msg").css({ color: 'red' });
+                        document.getElementById('error_msg').innerHTML = "Schedule/Subject Already Taken!";
+                    }
+                    
                 }
-                
-            }
-        });
+            });
+            
+        }
+    });
+};
 
+// function when user clicked on the schedule
+dp.onEventClick = function(args) {
+    var start = args.e.id();
+    $.ajax({
+        type: "POST",
+        url: "php/functions.php",
+        data: {
+            show_schedule: start
+        },
+        success: function(result) {
+            var schedule = eval(result);
+            document.getElementById('course_code').innerHTML = "<b>Course Code:</b> " + schedule[0];
+            document.getElementById('course_description').innerHTML = "<b>Course Description:</b> " + schedule[1];
+            document.getElementById('professor').innerHTML = "<b>Faculty:</b> " + schedule[2];
+            document.getElementById('class').innerHTML = "<b>Class:</b> " + schedule[3];
+            document.getElementById('room').innerHTML = "<b>Room:</b> " + schedule[4];
+            document.getElementById('schedule').innerHTML = "<b>Schedule:</b> " + schedule[5];      
+            $("#myModal").modal();
 
-    
-    
+            
+        }
+    });
+};
+
+//////////////////////////////////////////////////////////////   
+/////////////////////////////////////////////////////////////
 $(document).ready(function() {
     var url = window.location.href;
     var filename = url.substring(url.lastIndexOf('/')+1);
@@ -173,6 +171,8 @@ $(document).ready(function() {
                 var option = '<option  value="' + professor[n] + '"> ' + professor[n + 1] + ' </option>';
                 n = n + 2;
                 $("#select-prof").append(option);
+                $("#select-prof-view").append(option);
+
             }
         },
         error: function (result) {
@@ -196,6 +196,8 @@ $(document).ready(function() {
                 var option = '<option  value="' + class_name[n] + '"> ' + class_name[n + 1] + ' </option>';
                 n = n + 2;
                 $("#select-class").append(option);
+                $("#select-class-view").append(option);
+
             }
         },
         error: function (result) {
@@ -218,6 +220,8 @@ $(document).ready(function() {
                 var option = '<option  value="' + room[n] + '"> ' + room[n + 1] + ' </option>';
                 n = n + 2;
                 $("#select-room").append(option);
+                $("#select-room-view").append(option);
+
             }
         },
         error: function (result) {
